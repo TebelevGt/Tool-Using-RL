561bdddfa34e0f647d6d9eb33d056addf362d3fc
George Tebelev <tebelev99@mail.ru>
2026-03-13 11:26:48 +0300

    Add evaluate_agent import to enhance evaluation capabilities in the environment

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,3 +1,4 @@
 from .pendolf_env import PendolfEnv, PendolfDataset, grpo_env_reward_func
 from .prompts import SYSTEM_PROMPT
 from .utils import get_pendolf_dataset
+from .evaluation import evaluate_agent
